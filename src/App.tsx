8a17e11ca7ada57065b2b8706cf44ca7306b668c
alejandro velazco <alejanvelazco2008@hotmail.com>
2022-05-22 11:01:31 +0200

    add new fixs

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import './App.css';
 import {Provider} from "react-redux";
 import store from "./store";
 import {Routes} from "./routes";
+import {GlobalStyles, ThemeProvider} from "@mui/material";
+import {darkTheme} from "./styles/theme";
+import {globalStyles} from "./styles/global";
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+        <ThemeProvider theme={darkTheme} >
+            <Routes />
+            <GlobalStyles styles={globalStyles} />
+        </ThemeProvider>
     </Provider>
   );
 }
