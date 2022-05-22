3a4e933edc475d1ba56544913847c99c12447ab4
alejandro velazco <alejanvelazco2008@hotmail.com>
2022-05-22 11:17:25 +0200

    add new fix

---
+++ src/components/organism/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import React, {FC} from "react";
+import {NavbarStyles} from "./styles";
+
+
+export const Navbar:FC = () => {
+    return (
+        <NavbarStyles>
+
+        </NavbarStyles>
+    )
+}
